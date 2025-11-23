e148a519e1fd5f030d9d751e91d8b2fb778267a2
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-11-23 19:28:30 +0300

    add

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_path = file_name.with_suffix('.txt') if isinstance(file_name, Path) else file_name + ".txt"
+    with open(file_path, "w") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = file_name.with_suffix('.txt') if isinstance(file_name, Path) else file_name + ".txt"
+    with open(file_path, "a") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = file_name.with_suffix('.txt') if isinstance(file_name, Path) else file_name + ".txt"
+    with open(file_path, "r") as f:
+        content = f.read()
+    return content
